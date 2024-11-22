27aeca7c83a0ec4cbd28c14e263ff0771cb8fa80
Bruno Silva <gomesdasilva@caliza.com>
2024-11-22 12:01:53 -0300

    add: trapping water

---
+++ src/main/java/lesson1/TrappingRainWater.java
@@ -0,0 +1,42 @@
+package lesson1;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+import static precondition.Preconditions.check;
+
+public class TrappingRainWater {
+
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int result = monotonicStack(height);
+        check(result == 6);
+        System.out.println(result);
+
+        int[] height2 = {4,2,0,3,2,5};
+        result = monotonicStack(height2);
+        check(result == 9);
+        System.out.println(result);
+    }
+
+    public static int monotonicStack(int[] height) {
+        Deque<Integer> stack = new LinkedList<>();
+        int water = 0;
+
+        for (int i = 0; i < height.length; i++) {
+            int currentHeight = height[i];
+            while (!stack.isEmpty() && height[stack.peek()] < currentHeight) {
+                int top = height[stack.pop()];
+                if (!stack.isEmpty()) {
+                    int maxLeft = height[stack.peek()];
+                    int maxRight = height[i];
+                    int width = i - stack.peek() - 1;
+                    int wallHeight = Math.min(maxRight, maxLeft) - top;
+                    water += wallHeight * width;
+                }
+            }
+            stack.push(i);
+        }
+        return water;
+    }
+}

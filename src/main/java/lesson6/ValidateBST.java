8662a06b0ef874ebd8bf00287e1b04ae6f7e4d34
Bruno Silva <gomesdasilva@caliza.com>
2024-12-04 21:03:56 -0300

    add: validate bst solution

---
+++ src/main/java/lesson6/ValidateBST.java
@@ -0,0 +1,37 @@
+package lesson6;
+
+public class ValidateBST {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        long max = (long)Integer.MAX_VALUE + 1;
+        long min = (long)Integer.MIN_VALUE -1;
+        return isValidBST(root, min, max);
+    }
+
+    public boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+}

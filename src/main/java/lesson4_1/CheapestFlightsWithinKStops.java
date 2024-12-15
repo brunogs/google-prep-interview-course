df64f9cee151bf300b4215c3c9dc4da04a065ca0
Bruno Silva <gomesdasilva@caliza.com>
2024-12-15 15:28:33 -0300

    fix: the use of dijkstra, but it's not the desired solution yet

--- src/main/java/lesson4_1/CheapestFlightsWithinKStops.java
+++ src/main/java/lesson4_1/CheapestFlightsWithinKStops.java
@@ -1,8 +1,6 @@
 package lesson4_1;
 
-import java.util.HashSet;
 import java.util.PriorityQueue;
-import java.util.Set;
 
 public class CheapestFlightsWithinKStops {
 
@@ -12,53 +10,33 @@ public class CheapestFlightsWithinKStops {
 
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
 
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
-        Set<Integer> visited = new HashSet<>();
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> Integer.compare(a[1], b[1]));
+        heap.add(new int[]{src, 0, 0});
 
-        int cost = 0;
-        Integer result = null;
+        while (!heap.isEmpty()) { //0
 
-        /*
-        [[0,1,2],[1,2,1],[2,0,10]]
-        k = 1
-        src = 1
-        dst = 2
+            int[] current = heap.poll();
 
-        cost = 3
-        heap = [4,5], [1, 9]
-        */
+            int node = current[0];
+            int cost = current[1];
+            int stops = current[2];
 
-        while (k >= 0 || !heap.isEmpty()) { //0
-
-            if (k >= 0) {
-                nextPath(n, flights, src, heap, cost);
+            if (node == dst) {
+                return cost;
             }
 
-            if (heap.isEmpty()) {
-                return -1;
+            if (stops > k) {
+                continue;
             }
 
-            int[] bestPath = heap.poll();
-            src = bestPath[0];
-            cost = bestPath[1];
-            //System.out.println(src + " _ cost = " + cost + " k = " + k);
-
-            if (src == dst) {
-                result = result == null ? cost : Math.min(result, cost);
-            }
-            k--;
-        }
-
-        // check if the dst is in the heap
-
-        return result != null ? result : -1;
-    }
-
-    private void nextPath(int n, int[][] flights, int src, PriorityQueue<int[]> heap, int currentCost) {
-        for (int i = 0; i < flights.length; i++) {
-            if (flights[i][source] == src) {
-                heap.add(new int[]{flights[i][target], currentCost + flights[i][price]});
+            // next paths
+            for (int[] flight : flights) {
+                if (flight[source] == node) {
+                    heap.add(new int[]{flight[target], cost + flight[price], stops + 1});
+                }
             }
         }
+
+        return -1;
     }
 }

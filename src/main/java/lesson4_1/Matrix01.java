bd114c294c657aad7f80560e5e0aaa273d04441e
Bruno Silva <gomesdasilva@caliza.com>
2024-12-14 22:56:06 -0300

    fix: 01 matrix solution

--- src/main/java/lesson4_1/Matrix01.java
+++ src/main/java/lesson4_1/Matrix01.java
@@ -16,55 +16,55 @@ public class Matrix01 {
     }
 
     public static int[][] updateMatrix(int[][] mat) {
-        /*
-        [
-         0   [0,0,0],
-         1   [0,1,0],
-         2   [1,1,1]
-        ]
-        */
+        boolean[][] visited = new boolean[mat.length][mat[0].length];
+
+        Queue<int[]> queue = new LinkedList<>();
+
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                if (mat[i][j] == 1) {// 2, 0
-                    //visited[i][j] = true;
-                    int count = countNearestZero(mat, i, j);
-                    System.out.println("positions [" + i + ", " + j + "] = " + count);
-                    mat[i][j] = count;
+                if (mat[i][j] == 0 && !visited[i][j]) {
+                    queue.add(new int[]{i, j, 1});
                 }
             }
         }
+
+        fillNonZeroPosition(mat, queue, visited);
+
         return mat;
     }
 
-    public static int countNearestZero(int[][] mat, int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int count = 0;
+    /*
+    [
+        [0,0,0],
+        [0,1,0],
+        [1,1,1]
+    ]
+    */
+    public static void fillNonZeroPosition(int[][] mat, Queue<int[]> queue, boolean[][] visited) {
 
-        queue.add(new int[]{i, j});
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        // 1
 
         while (!queue.isEmpty()) {
-            count++; //1
-            int layer = queue.size(); //1
-            while (layer > 0) {
-                int[] position = queue.poll();
-                int x = position[0]; //2
-                int y = position[1]; //0
-                if (mat[x][y] == 0) {
-                    System.out.println(count + " position " + x + ", " + y + " = " + mat[x][y]);
-                    return Math.max(0, count-1);
-                }
+            int[] position = queue.poll();
+            int x = position[0];
+            int y = position[1];
+            int currentDistance = position[2];
 
-                for (int idx = 0; idx < directions.length; idx++) {
-                    int newX = x + directions[idx][0]; //2
-                    int newY = y + directions[idx][1]; //1
-                    if (newX >= 0 && newX < mat.length && newY >= 0 && newY < mat[newX].length) {
-                        queue.add(new int[]{newX, newY}); //[1,0], [2,1]
-                    }
+            if (mat[x][y] != 0) {
+                mat[x][y] = currentDistance;
+            }
+
+            for (int idx = 0; idx < directions.length; idx++) {
+                int newX = x + directions[idx][0];
+                int newY = y + directions[idx][1];
+                boolean validGridPosition = newX >= 0 && newX < mat.length && newY >= 0 && newY < mat[newX].length;
+                if (validGridPosition && !visited[newX][newY] && mat[newX][newY] == 1) {
+                    visited[newX][newY] = true;
+                    int distance = mat[x][y]+1;
+                    queue.add(new int[]{newX, newY, distance});
                 }
-                layer--;
             }
         }
-        return Math.max(0, count-1);
     }
 }
